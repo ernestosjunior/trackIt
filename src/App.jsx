7406250168579f13b5769b4e3f972f246f25bbef
Ernesto Junior <ernesto.sjunior@hotmail.com>
2022-08-06 00:00:31 -0300

    feat: create link component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { Button, Input } from "./components";
+import { Button, Input, Link } from "./components";
 
 function App() {
   return (
     <>
       <Input type="text" placeholder="Seu nome aqui..." />
       <Button label="Enviar" loading={false} disabled />
+      <Link label="Crie uma conta" path={""} />
     </>
   );
 }
---
+++ src/components/RedirectButton/styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const StyledLink = styled.span`
+  font-weight: 400;
+  font-size: 13.976px;
+  line-height: 17px;
+  text-align: center;
+  text-decoration-line: underline;
+  color: #52b6ff;
+  cursor: pointer;
+`;
